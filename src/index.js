4b8c62b70612ab11d464f4023069ad6fd9535ec7
Aarohi <aarohi.verma@metasquaretech.com>
2025-10-09 10:43:18 +0530

    project setup

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+const PORT = process.env.PORT || 3000;
+const corsOptions = {
+  origin: "http://localhost:5173",
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
