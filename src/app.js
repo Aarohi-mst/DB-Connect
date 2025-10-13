b36b63c3cf2265687eb9f9995fef708757f6ec26
Aarohi <aarohi.verma@metasquaretech.com>
2025-10-13 14:49:34 +0530

    complete setup with controller and routes

--- src/app.js
+++ src/app.js
@@ -13,4 +13,8 @@ app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+//routes import
+import userRoutes from "./routes/user.routes.js";
+//routes declaration
+app.use("/api/v1/users", userRoutes);
 export default app;
