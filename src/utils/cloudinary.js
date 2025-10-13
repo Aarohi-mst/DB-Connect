dd58965c34bb86f75fe4a043506b3a8a31889be0
Aarohi <aarohi.verma@metasquaretech.com>
2025-10-13 12:38:11 +0530

    upload file on backend/multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,24 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnClodinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    //uploading file on cloudinary
+    response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    //file has been uploaded successfully
+    console.log("file is uploaded on clodinary", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); //remove locally saved temporary file as upload operation got failed
+    return null;
+  }
+};
+export default uploadOnClodinary;
